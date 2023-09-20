4800f4d2c1e058bc0499dd3013d9e19456a16b91
Ralf Spöth <ralf.spoeth@profidata.com>
2023-09-20 18:38:08 +0200

    update to jdk-21

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module com.github.ralfspoeth.json {
     exports com.github.ralfspoeth.json;
     exports com.github.ralfspoeth.json.io;
-    exports com.github.ralfspoeth.json.query;
+    // too immature... exports com.github.ralfspoeth.json.query;
 }

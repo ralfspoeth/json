4e8e71273a9419bf6ccb94be69220ef712550436
Ralf Spöth <ralf.spoeth@profidata.com>
2023-10-10 15:14:04 +0200

    little test

--- src/test/java/com/github/ralfspoeth/json/ElementTest.java
+++ src/test/java/com/github/ralfspoeth/json/ElementTest.java
@@ -1,10 +1,10 @@
 package com.github.ralfspoeth.json;
 
-import com.github.ralfspoeth.json.io.JsonWriter;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -21,7 +21,7 @@ class ElementTest {
                 )
         );
         var obj = je.elements().stream()
-                .filter(e->e instanceof JsonObject)
+                .filter(e -> e instanceof JsonObject)
                 .map(JsonObject.class::cast)
                 .findFirst()
                 .orElseThrow();
@@ -32,5 +32,31 @@ class ElementTest {
                 () -> assertThrows(UnsupportedOperationException.class, () -> obj.members().put("c", JsonBoolean.TRUE))
 
         );
+    }
+
+    @Test
+    void testSwitch() {
+        var str = Stream.of(
+                JsonBoolean.FALSE,
+                JsonBoolean.TRUE,
+                JsonNumber.ZERO,
+                JsonNull.INSTANCE,
+                new JsonString("str"),
+                Element.arrayBuilder()
+                        .item(JsonNumber.ZERO)
+                        .build(),
+                Element.objectBuilder()
+                        .named("a", JsonBoolean.TRUE)
+                        .named("b", Element.arrayBuilder())
+                        .build()
+        );
+        var l = str.map(e -> switch (e) {
+            case JsonBoolean.TRUE -> "true";
+            case JsonBoolean.FALSE -> "false";
+            case JsonNull _ -> "null";
+            case Basic b -> b.json();
+            case Aggregate a -> a.toString();
+        }).toList();
+        System.out.println(l);
     }
 }

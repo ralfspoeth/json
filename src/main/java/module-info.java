38371a62fad48d07b1a782ffc8e5a4d3cf827aaf
Ralf Spöth <ralf.spoeth@profidata.com>
2023-12-19 15:12:34 +0100

    switched to version 1.0.5 of basix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,4 @@ module io.github.ralfspoeth.json {
     requires io.github.ralfspoeth.basix;
     exports io.github.ralfspoeth.json;
     exports io.github.ralfspoeth.json.io;
-    // too immature... exports com.github.ralfspoeth.json.query;
 }

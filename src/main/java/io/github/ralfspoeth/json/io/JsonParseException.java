1344c11e77c0f86c08bdd5ef03e37bb89cfe3410
Ralf Spöth <ralf.spoeth@profidata.com>
2025-05-23 00:09:26 +0200

    overriding message

--- src/main/java/io/github/ralfspoeth/json/io/JsonParseException.java
+++ src/main/java/io/github/ralfspoeth/json/io/JsonParseException.java
@@ -3,17 +3,14 @@ package io.github.ralfspoeth.json.io;
 public class JsonParseException extends RuntimeException {
     private final int row, column;
 
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-
     public JsonParseException(String message, int row, int column) {
         super(message);
         this.row = row;
         this.column = column;
+    }
+
+    @Override
+    public String getMessage() {
+        return "%s at row %d, column %d".formatted(super.getMessage(), row, column);
     }
 }

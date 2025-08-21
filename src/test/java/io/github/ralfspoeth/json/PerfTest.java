6e97cb6935465a40e8bf03a389f856356811c034
Ralf Spöth <ralf.spoeth@profidata.com>
2025-08-21 20:25:00 +0200

    valhalla test bed

---
+++ src/test/java/io/github/ralfspoeth/json/PerfTest.java
@@ -0,0 +1,34 @@
+package io.github.ralfspoeth.json;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.zip.GZIPInputStream;
+
+import static java.util.Objects.requireNonNull;
+
+class PerfTest {
+
+    private Reader open() throws IOException {
+        return new InputStreamReader(new GZIPInputStream(
+                requireNonNull(getClass().getResourceAsStream("/very-big-array.json.gz"))
+        ));
+    }
+
+    @Test
+    void testVeryBigArray() throws IOException {
+        try (Reader reader = open()) {
+            long start = System.currentTimeMillis();
+            var array = Greyson.readValue(reader);
+            var len = switch(array) {
+                case JsonArray(var elems) -> elems.size();
+                case JsonObject(var map) -> map.size();
+                case JsonValue _ -> 1;
+            };
+            long end = System.currentTimeMillis();
+            System.out.println("Parsing took " + (end - start) + " ms to read " + len + " elements.");
+        }
+    }
+}

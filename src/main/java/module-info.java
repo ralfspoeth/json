196e44125901b1aa9230e62815f7a64bf67c5c6a
ralfspoeth <ralf.spoeth@gmail.com>
2026-04-14 17:35:32 +0100

    exporting the io package again

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module io.github.ralfspoeth.greyson {
     requires io.github.ralfspoeth.basix;
     requires static org.jspecify;
     exports io.github.ralfspoeth.json;
+    exports io.github.ralfspoeth.json.io;
     exports io.github.ralfspoeth.json.query;
     exports io.github.ralfspoeth.json.data;
 }
